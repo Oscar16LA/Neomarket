090e158f02ad30ca25cb9fe05bc997e56b47fdcc
RL01521 <161052814+RL01521@users.noreply.github.com>
2024-11-11 22:13:08 -0600

    Delete extras

--- prueba_15/Neomarket/productos/forms.py
+++
@@ -1,8 +0,0 @@
-# archivo forms.py
-from django import forms
-from .models import Producto
-
-class MiFormulario(forms.ModelForm):
-    class Meta:
-        model = Producto
-        fields = ['campo1', 'campo2']
